8f4a973a203d8fc25e19a5ccab3959fd607ef39a
Chris Boakes <chris@chrisboakes.com>
2020-03-25 16:18:46 +0000

    Dynamic politics content

--- src/routes/politics/index.js
+++ src/routes/politics/index.js
@@ -2,10 +2,29 @@ import { h, Component } from 'preact';
 import style from './style';
 
 export default class Politics extends Component {
-	render() {
+	async componentDidMount() {
+		const res = await fetch('https://my-json-server.typicode.com/chrisboakes/preact-demo/politics-articles');
+		const data = await res.json();
+		let content = '';
+
+		if (data.status === 200) {
+			data.forEach( result => {
+				content += result.title;
+			});
+		} else {
+			content = 'No results';
+		}
+
+		this.setState({ content });
+	}
+
+	render({}, { content='Loading' }) {
 		return (
 			<div class={style.politics}>
 				<h1>Politics</h1>
+				<ul>
+					{content}
+				</ul>
 			</div>
 		);
 	}
