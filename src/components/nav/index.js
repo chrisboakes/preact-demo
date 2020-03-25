4f1f3201f9c8900b624a83a5a20d76e00987f552
Chris Boakes <chris@chrisboakes.com>
2020-03-25 10:59:36 +0000

    Basic header styling

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,9 +2,11 @@ import style from "./style.css";
 import { Link } from 'preact-router/match';
 
 export default () => (
-	<nav class={style.header}>
-		<Link activeClassName={style.active} href="/">Home</Link>
-		<Link activeClassName={style.active} href="/politics">Politics</Link>
-		<Link activeClassName={style.active} href="/news">News</Link>
+	<nav class={style.nav}>
+		<ul>
+			<li><Link activeClassName={style.active} href="/">Home</Link></li>
+			<li><Link activeClassName={style.active} href="/politics">Politics</Link></li>
+			<li><Link activeClassName={style.active} href="/news">News</Link></li>
+		</ul>
 	</nav>
 );
