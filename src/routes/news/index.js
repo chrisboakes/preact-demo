d722b39d8ad7133081577cab28864d71dcaa498c
Chris Boakes <chris@chrisboakes.com>
2020-03-31 15:24:12 +0100

    Switched around politics and news

--- src/routes/news/index.js
+++ src/routes/news/index.js
@@ -1,9 +1,41 @@
 import { h, Component } from 'preact';
+import style from './style';
 
-export default class News extends Component {
-	render() {
+export default class Politics extends Component {
+	async componentDidMount() {
+		const res = await fetch('https://my-json-server.typicode.com/chrisboakes/preact-demo/news-articles');
+		const data = await res.json();
+		let content = [];
+
+		if (res.status === 200) {
+			data.forEach(result => {
+				result.link = `${result.section}/${result.slug}`;
+				content.push(result);
+			});
+		} else {
+			content = ['No results'];
+		}
+
+		this.setState({ content });
+	};
+
+	render({ }, { content = [{ title: 'Loading' }] }) {
 		return (
-			<div>News Page</div>
+			<div class={style.politics}>
+				<h1>News</h1>
+				<h2>This page is rendered client-side</h2>
+				<ul>
+					{
+						content.map(item => (
+							<li>
+								{item.link ? (
+									<a href={item.link}>{item.title}</a>
+								) : item.title}
+							</li>
+						))
+					}
+				</ul>
+			</div>
 		);
-	}
+	};
 }
--- src/routes/politics/index.js
+++ src/routes/politics/index.js
@@ -1,39 +1,10 @@
 import { h, Component } from 'preact';
-import style from './style';
 
 export default class Politics extends Component {
-	async componentDidMount() {
-		const res = await fetch('https://my-json-server.typicode.com/chrisboakes/preact-demo/politics-articles');
-		const data = await res.json();
-		let content = [];
-
-		if (res.status === 200) {
-			data.forEach( result => {
-				result.link = `${ result.section }/${ result.slug }`;
-				content.push(result);
-			});
-		} else {
-			content = ['No results'];
-		}
-
-		this.setState({ content });
-	};
-
-	render({}, { content=[{title: 'Loading'}] }) {
+	render() {
 		return (
-			<div class={style.politics}>
+			<div>
 				<h1>Politics</h1>
-				<ul>
-					{
-						content.map( item => (
-							<li>
-								{ item.link ? (
-									<a href={ item.link }>{item.title}</a>
-								) : item.title  }
-							</li>
-						))
-					}
-				</ul>
 			</div>
 		);
 	};
